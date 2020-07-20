5af43c6cd06ab24fc1b23f96e8939a6f8d1e270f
Anthony Sena <asena5@its.jnj.com>
2020-07-20 12:44:22 -0400

    Update premerge

--- extras/PreMergeDiagnostics.R
+++ extras/PreMergeDiagnostics.R
@@ -1,4 +1,4 @@
-outputFolder <- "E:/SkeletonCharybdis/Runs/CDM_OPTUM_EHR_COVID_v1239/diagnostics"
+outputFolder <- "E:/SkeletonCharybdis/Diagnostics"
 CohortDiagnostics::preMergeDiagnosticsFiles(file.path(outputFolder,"target"))
 CohortDiagnostics::preMergeDiagnosticsFiles(file.path(outputFolder,"feature"))
 CohortDiagnostics::preMergeDiagnosticsFiles(file.path(outputFolder,"subgroup"))
